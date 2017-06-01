6da56a1895cac98556d3e38c38cd93b22abbb7f9
txie936 <txie936@aucklanduni.ac.nz>
2017-06-01 12:02:15 +1200

    xie

--- src/code_project/Servlet/AlbumsChangeServlet.java
+++ src/code_project/Servlet/AlbumsChangeServlet.java
@@ -22,6 +22,8 @@ import javax.servlet.http.HttpSession;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
@@ -222,11 +224,7 @@ private void deleteAlbumsImage(HttpServletRequest request,HttpServletResponse re
                     tags=fi.getString();
                 }
             }
-
-
         }
-
-
         ArticleInfo articleInfo = new ArticleInfo(title, content + imageAddress, tags);
         request.setAttribute("articleInfo", articleInfo);
         request.setAttribute("information", "Success");
@@ -235,6 +233,42 @@ private void deleteAlbumsImage(HttpServletRequest request,HttpServletResponse re
     }
 
 
+
+
+    public void createVideo() throws Exception{
+
+
+        File file = new File(".mp4");
+        FileInputStream fin = new FileInputStream(file);
+        byte b[] = new byte[(int)file.length()];
+        fin.read(b);
+        File nf = new File("D:/K.mp4");
+        FileOutputStream fw = new FileOutputStream(nf);
+        fw.write(b);
+        fw.flush();
+        fw.close();
+
+    }
+    public void createAudio() throws Exception{
+
+
+        File file = new File(".mp3");
+
+        FileInputStream fin = new FileInputStream(file);
+
+        byte b[] = new byte[(int)file.length()];
+
+        fin.read(b);
+
+        File nf = new File("D:/K.mp3");
+
+        FileOutputStream fw = new FileOutputStream(nf);
+
+        fw.write(b);
+        fw.flush();
+        fw.close();
+
+    }
     private static String getCurrentTimeStamp() {
         return new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
     }
