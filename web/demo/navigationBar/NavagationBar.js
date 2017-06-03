29704ca4074c7f2812284653ce105eeaf88945a0
yzhb363 <tvxq8553308>
2017-06-03 17:44:24 +1200

    newFile

--- web/demo/navigationBar/NavagationBar.js
+++ web/demo/navigationBar/NavagationBar.js
@@ -1,3 +1,9 @@
 /**
  * Created by yzhb363 on 3/06/2017.
  */
+$(document).ready(function () {
+    // create sidebar and attach to menu open
+    $('.ui.sidebar')
+        .sidebar('attach events', '.toc.item')
+    ;
+});
