777172d44b996d5b19ca4b74cdc692d0b381b395
Qian Peng <qpen546@aucklanduni.ac.nz>
2017-06-12 23:27:59 +1200

    restructure database

--- web/WEB-INF/DatabaseSetting.sql
+++ web/WEB-INF/DatabaseSetting.sql
@@ -1,48 +1,131 @@
-DROP TABLE IF EXISTS Comment;
-DROP TABLE IF EXISTS Article;
-DROP TABLE IF EXISTS UserInfo;
+DROP TABLE IF EXISTS CommentReply_beta_1;
+DROP TABLE IF EXISTS Comment_beta_1;
+DROP TABLE IF EXISTS Article_beta_1;
+DROP TABLE IF EXISTS UserInfo_beta_1;
 
-CREATE TABLE UserInfo (
-  username  VARCHAR(99) PRIMARY KEY,
-  password  BLOB,
-  salt      BLOB,
-  firstName VARCHAR(99),
-  lastName  VARCHAR(99),
-  email     VARCHAR(99),
-  birthDate DATE,
-  gender    VARCHAR(99),
-  avatar    VARCHAR(999)
+CREATE TABLE UserInfo_beta_1 (
+  userID     BIGINT AUTO_INCREMENT NOT NULL UNIQUE,
+  username   VARCHAR(32) UNIQUE,
+  password   BLOB,
+  salt       BLOB,
+  iterations BIGINT,
+  firstName  VARCHAR(32),
+  lastName   VARCHAR(32),
+  email      VARCHAR(32),
+  birthDate  DATE,
+  gender     VARCHAR(32),
+  avatar     VARCHAR(256),
+  googleID   BIGINT UNIQUE,
+  PRIMARY KEY (username, avatar)
 );
 
-DROP TABLE IF EXISTS Article;
+DROP TABLE IF EXISTS Article_beta_1;
 
-CREATE TABLE Article (
-  articleID  INT PRIMARY KEY AUTO_INCREMENT,
-  title      VARCHAR(50),
-  content    VARCHAR(5000),
+CREATE TABLE Article_beta_1 (
+  articleID  BIGINT PRIMARY KEY AUTO_INCREMENT,
+  title      VARCHAR(99),
+  content    LONGTEXT,
   postTime   DATETIME,
-  tags       VARCHAR(99),
-  username   VARCHAR(99),
-  userAvatar VARCHAR(999)
+  tags       VARCHAR(32),
+  username   VARCHAR(32),
+  userAvatar VARCHAR(256),
+  FOREIGN KEY (username, userAvatar) REFERENCES UserInfo_beta_1 (username, avatar)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS Comment;
+DROP TABLE IF EXISTS Comment_beta_1;
 
-CREATE TABLE Comment (
-  commentID INT PRIMARY KEY AUTO_INCREMENT,
-  content   VARCHAR(500),
-  postTime  DATETIME,
-  username  VARCHAR(99),
-  articleID INT
+CREATE TABLE Comment_beta_1 (
+  commentID  BIGINT PRIMARY KEY AUTO_INCREMENT,
+  content    TINYTEXT,
+  postTime   DATETIME,
+  username   VARCHAR(32),
+  userAvatar VARCHAR(256),
+  articleID  BIGINT,
+  FOREIGN KEY (username, userAvatar) REFERENCES UserInfo_beta_1 (username, avatar)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (articleID) REFERENCES Article_beta_1 (articleID)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
 
-DROP TABLE IF EXISTS CommentReply;
+DROP TABLE IF EXISTS CommentReply_beta_1;
 
-CREATE TABLE CommentReply (
-  commentReplyID INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE CommentReply_beta_1 (
+  commentReplyID BIGINT PRIMARY KEY AUTO_INCREMENT,
   content        VARCHAR(500),
   postTime       DATETIME,
-  username       VARCHAR(99),
-  commentID      INT
+  username       VARCHAR(32),
+  userAvatar     VARCHAR(256),
+  articleID      BIGINT,
+  commentID      BIGINT,
+  FOREIGN KEY (username, userAvatar) REFERENCES UserInfo_beta_1 (username, avatar)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (articleID) REFERENCES Article_beta_1 (articleID)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (commentID) REFERENCES Comment_beta_1 (commentID)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+
+DROP TABLE IF EXISTS AlbumsImage_beta_1;
+
+CREATE TABLE IF NOT EXISTS AlbumsImage_beta_1 (
+  id       INT AUTO_INCREMENT,
+  fileName VARCHAR(50),
+  username CHAR(10),
+  address  VARCHAR(1000) NOT NULL,
+  postTime DATETIME,
+  PRIMARY KEY (id),
+  FOREIGN KEY (username) REFERENCES UserInfo_beta_1 (username)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS AlbumsVideo_beta_1;
+
+CREATE TABLE IF NOT EXISTS AlbumsVideo_beta_1 (
+  id       INT AUTO_INCREMENT,
+  fileName VARCHAR(50),
+  username CHAR(10),
+  address  VARCHAR(1000) NOT NULL,
+  postTime DATETIME,
+  PRIMARY KEY (id),
+  FOREIGN KEY (username) REFERENCES UserInfo_beta_1 (username)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS AlbumsAudio_beta_1;
+
+CREATE TABLE IF NOT EXISTS AlbumsAudio_beta_1 (
+  id       INT AUTO_INCREMENT,
+  fileName VARCHAR(50),
+  username CHAR(10),
+  address  VARCHAR(1000) NOT NULL,
+  postTime DATETIME,
+  PRIMARY KEY (id),
+  FOREIGN KEY (username) REFERENCES UserInfo_beta_1 (username)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS UserRelationship_beta_1;
+
+CREATE TABLE IF NOT EXISTS UserRelationship_beta_1 (
+  follower VARCHAR(99) NOT NULL,
+  follow   VARCHAR(99) NOT NULL,
+  PRIMARY KEY (follow, follower),
+  FOREIGN KEY (follow) REFERENCES UserInfo_beta_1 (username)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (follower) REFERENCES UserInfo_beta_1 (username)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
